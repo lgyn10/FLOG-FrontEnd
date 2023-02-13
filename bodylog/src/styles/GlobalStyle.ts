d38ae32d280055085b74af5479d19e33d29f089a
ChanGyun Lee <72643542+lgyn10@users.noreply.github.com>
2023-02-13 16:51:05 +0900

    build: 디렉토리 정리 및 styled-components 설치 (#3)

---
+++ bodylog/src/styles/GlobalStyle.ts
@@ -0,0 +1,14 @@
+// Gloval style 지정
+
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+*{
+  margin:0;
+  padding:0;
+  box-sizing: border-box;
+  color: red;
+}
+`;
+
+export default GlobalStyle;
